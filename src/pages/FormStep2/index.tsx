fd98359a4d1781a6d127bceccc274d1d0d976343
Art <arthurluiz36@gmail.com>
2021-12-28 21:01:58 -0200

    Estruturacao do step2, componente de selecao para escolha de experiencia

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,9 +1,56 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import {useForm, FormAction} from '../../Context/FormContext'
+import {Theme} from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
+import { SelectOption } from '../../components/SelectOption'
 
 export const FormStep2 = () =>{
+
+    const history = useNavigate();
+
+    const {state, dispatch} = useForm();
+
+    const handleNextStep = () =>{
+        if(state.name !== ''){
+            history('/step2')
+        } else {
+            alert("Preencha seus dados")
+        }
+    };
+
+    useEffect(() =>{
+        dispatch({
+            type: FormAction.setCurrentStep,
+            payload: 2
+        })
+    }, [])
+    
+
     return(
-        <C.Container>
-            Passo 2
-        </C.Container>
+
+        <Theme>
+            <C.Container>
+            <p>Passo 2/3 </p>
+            <h1>Vamos começar com seu nome</h1>
+            <p>Preencha abaixo com seu nome completo.</p>
+
+            <hr/>
+
+            <SelectOption 
+                title = "Iniciante"
+                description = "Programo há menos de 2 anos"
+                icon = "🤓"
+            />
+             <SelectOption 
+                title = "Veterano"
+                description = "Programo há mais de 2 anos"
+                icon = "😎"
+            />
+            
+
+            <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
     );
 }
