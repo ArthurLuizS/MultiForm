e452af2269f86f4e136820e9c8503b05a578437f
Art <arthurluiz36@gmail.com>
2021-12-27 22:53:13 -0200

    Componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as C from './styles';
+
+export const Header = () =>{
+
+    return(
+        <C.Container>
+         <h1>Cadastro de Desenvolvedor</h1>
+         <p>Realize seu cadastro</p>
+        </C.Container>
+    );
+}
