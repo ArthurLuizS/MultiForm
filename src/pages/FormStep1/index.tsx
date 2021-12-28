200e447ebfc954e48e441c8addd5437f97a70297
Art <arthurluiz36@gmail.com>
2021-12-28 19:37:40 -0200

    Adicionado conteudo ao step1, e utilizando o contexto, para setar name e useeffect para mudar o CurrentStep

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,53 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import {useForm, FormAction} from '../../Context/FormContext'
 import {Theme} from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
 
 export const FormStep1 = () =>{
+
+    const history = useNavigate();
+
+    const {state, dispatch} = useForm();
+
+    const handleNextStep = () =>{
+            history('/step2')
+    };
+
+    useEffect(() =>{
+        dispatch({
+            type: FormAction.setCurrentStep,
+            payload: 1
+        })
+    }, [])
+    
+    const handleSetName = (e : ChangeEvent<HTMLInputElement>) =>{
+        dispatch({
+            type: FormAction.setName,
+            payload: e.target.value
+        })
+    }
+
     return(
+
         <Theme>
             <C.Container>
-            Passo 1
+            <p>Passo 1/3 </p>
+            <h1>Vamos começar com seu nome</h1>
+            <p>Preencha abaixo com seu nome completo.</p>
+
+            <hr/>
+
+            <label>
+                Nome completo: 
+                <input 
+                    type= "text"
+                    autoFocus
+                    onChange={handleSetName}
+                ></input>
+            </label>
+
+            <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
     );
