835d3715b277880c0e328f21c40f3b40dc8e718a
Art <arthurluiz36@gmail.com>
2021-12-29 21:30:20 -0200

    Adicionado o botao de voltar e  setado o dispatch do level atraves do click

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate , Link} from 'react-router-dom'
 import * as C from './styles'
 import {useForm, FormAction} from '../../Context/FormContext'
 import {Theme} from '../../components/Theme'
@@ -13,27 +13,37 @@ export const FormStep2 = () =>{
 
     const handleNextStep = () =>{
         if(state.name !== ''){
-            history('/step2')
+            history('/step3')
         } else {
             alert("Preencha seus dados")
         }
     };
 
     useEffect(() =>{
-        dispatch({
-            type: FormAction.setCurrentStep,
-            payload: 2
-        })
+        if (state.name === ''){
+            history('/')
+        }else{
+            dispatch({
+                type: FormAction.setCurrentStep,
+                payload: 2
+            })
+        }
     }, [])
     
+    const setLevel = (level: number) =>{
+        dispatch({
+            type: FormAction.setLevel,
+            payload: level
+        })
+    } 
 
     return(
 
         <Theme>
             <C.Container>
             <p>Passo 2/3 </p>
-            <h1>Vamos começar com seu nome</h1>
-            <p>Preencha abaixo com seu nome completo.</p>
+            <h1> {state.name}, o que melhor descreve você ? </h1>
+            <p>Escolha a opção que melhor condiz com seu estado atual, profissionalmente. </p>
 
             <hr/>
 
@@ -41,14 +51,19 @@ export const FormStep2 = () =>{
                 title = "Iniciante"
                 description = "Programo há menos de 2 anos"
                 icon = "🤓"
+                selected = {state.level === 0}
+                onClick = {()=>setLevel(0)}
             />
              <SelectOption 
                 title = "Veterano"
                 description = "Programo há mais de 2 anos"
                 icon = "😎"
+                selected = {state.level === 1}
+                onClick = {()=>setLevel(1)}
             />
             
-
+            <Link to="/" className='backButton'>Voltar</Link>
+            
             <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
