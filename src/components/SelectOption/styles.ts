a4412b0677476038fbd6ce8f283ac8734b7f3695
Art <arthurluiz36@gmail.com>
2021-12-29 21:32:44 -0200

    props para alterar o style do item selecionado

--- src/components/SelectOption/styles.ts
+++ src/components/SelectOption/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div< {selected: boolean} >`
     display: Flex;
-    border: 2px solid #16195C;
+    border: 2px solid ${props => props.selected? '#25CD89' : '#16195C' };
     border-radius: 10px;
     padding: 20px;
     margin-bottom: 15px;
@@ -13,11 +13,31 @@ export const Container = styled.div`
     }
 `
 
-export const Icon = styled.div``
+export const Icon = styled.div`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    background-color: #191A59;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
 
-export const Info = styled.div``
+`
 
-export const title = styled.div``
+export const Info = styled.div`
+    flex: 1;
+    margin-left: 20px;
+`
 
-export const Description = styled.div``
+export const title = styled.div`
+    font-size: 17px;
+    font-weight: bold;
+    margin-bottom: 7px;
+`
+
+export const Description = styled.div`
+    font-size: 14px;
+    color: #B8B8D4;
+`
 
